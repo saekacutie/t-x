46b59aaf46400903dffddc0e6b57e390726f5728
くんジェット <130436764+saekacutie@users.noreply.github.com>
2026-05-16 09:44:03 +0800

    Update tx_toolkit.py

--- tx_toolkit.py
+++ tx_toolkit.py
@@ -74,7 +74,7 @@ from colorama import init, Fore, Style
 init(autoreset=True)
 
 # ── CONFIG ──
-VERSION = "6.4"
+VERSION = "6.8"
 REPO_URL = "https://raw.githubusercontent.com/saekacutie/t-x/main/tx_toolkit.py"
 OWNER_SERVER = "https://request-tracker--mitsukitobashi.replit.app"
 APPROVED_FILE = os.path.expanduser("~/.tx_approved")
